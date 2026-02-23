7c356af980f0fa83bd7ce880da3fff349b15f8f6
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-02-23 22:01:42 +0530

    [Abhishek] : Added a LengthUnit.java file

--- src/main/java/com/quantity/domain/length/LengthUnit.java
+++ src/main/java/com/quantity/domain/length/LengthUnit.java
@@ -2,10 +2,10 @@ package com.quantity.domain.length;
 
 public enum LengthUnit {
 
-    FEET(1.0),                   
-    INCHES(1.0 / 12.0),        
-    YARDS(3.0),                 
-    CENTIMETERS(0.0328084);     
+    FEET(1.0),                  
+    INCHES(1.0 / 12.0),         
+    YARDS(3.0),                
+    CENTIMETERS(0.0328084);   
 
     private final double toFeetFactor;
 
