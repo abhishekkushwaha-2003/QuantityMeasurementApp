33b0f97d8733886733dc4200acb40cb9ace90d68
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-02-23 16:25:21 +0530

    [Abhishek] : Added a LengthUnit.java file

--- src/main/java/com/quantity/domain/length/LengthUnit.java
+++ src/main/java/com/quantity/domain/length/LengthUnit.java
@@ -1,20 +1,23 @@
-
 package com.quantity.domain.length;
 
 public enum LengthUnit {
 
-    FEET(1.0),
-    INCH(1.0 / 12.0),
-    YARD(3.0),
-    CENTIMETER(0.0328084167);
+    INCHES(1.0),
+    FEET(12.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
 
-    private final double conversionFactorToFeet;
+    private final double factorToInches;
 
-    LengthUnit(double conversionFactorToFeet) {
-        this.conversionFactorToFeet = conversionFactorToFeet;
+    LengthUnit(double factorToInches) {
+        this.factorToInches = factorToInches;
     }
 
-    public double toBaseUnit(double value) {
-        return value * conversionFactorToFeet;
+    public double toBase(double value) {
+        return value * factorToInches;
+    }
+
+    public double fromBase(double baseValue) {
+        return baseValue / factorToInches;
     }
 }
