dc281d137f766cedc89dc983f05e9dde054bf406
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-02-23 23:29:31 +0530

    [Abhishek] : Added a QuantityMeasurementApp.java file

--- src/main/java/com/quantity/app/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/app/QuantityMeasurementApp.java
@@ -1,24 +1,40 @@
 package com.quantity.app;
 
 import com.quantity.domain.Quantity;
+import com.quantity.unit.LengthUnit;
+import com.quantity.unit.WeightUnit;
 import com.quantity.unit.VolumeUnit;
 
 public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        Quantity<VolumeUnit> v1 = new Quantity<>(1.0, VolumeUnit.LITRE);
-        Quantity<VolumeUnit> v2 = new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
-        Quantity<VolumeUnit> v3 = new Quantity<>(1.0, VolumeUnit.GALLON);
+        Quantity<LengthUnit> l1 = new Quantity<>(10.0, LengthUnit.FEET);
+        Quantity<LengthUnit> l2 = new Quantity<>(6.0, LengthUnit.INCHES);
 
-        System.out.println("Equality:");
-        System.out.println(v1.equals(v2));
+        System.out.println("Subtraction (Length):");
+        System.out.println(l1.subtract(l2));
+        System.out.println(l1.subtract(l2, LengthUnit.INCHES));
 
-        System.out.println("\nConversion:");
-        System.out.println(v3.convertTo(VolumeUnit.LITRE));
+        System.out.println("\nDivision (Length):");
+        System.out.println(l1.divide(new Quantity<>(2.0, LengthUnit.FEET)));
 
-        System.out.println("\nAddition:");
-        System.out.println(v1.add(v2));
-        System.out.println(v1.add(v3, VolumeUnit.MILLILITRE));
+        Quantity<WeightUnit> w1 = new Quantity<>(10.0, WeightUnit.KILOGRAM);
+        Quantity<WeightUnit> w2 = new Quantity<>(5000.0, WeightUnit.GRAM);
+
+        System.out.println("\nSubtraction (Weight):");
+        System.out.println(w1.subtract(w2));
+
+        System.out.println("\nDivision (Weight):");
+        System.out.println(w1.divide(new Quantity<>(5.0, WeightUnit.KILOGRAM)));
+
+        Quantity<VolumeUnit> v1 = new Quantity<>(5.0, VolumeUnit.LITRE);
+        Quantity<VolumeUnit> v2 = new Quantity<>(500.0, VolumeUnit.MILLILITRE);
+
+        System.out.println("\nSubtraction (Volume):");
+        System.out.println(v1.subtract(v2));
+
+        System.out.println("\nDivision (Volume):");
+        System.out.println(v1.divide(new Quantity<>(10.0, VolumeUnit.LITRE)));
     }
 }
