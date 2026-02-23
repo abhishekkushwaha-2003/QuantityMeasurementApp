058305c5375273a2472b0c49e1f7557b56ff5e19
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-02-23 21:49:02 +0530

    [Abhishek] : Added a QuantityMeasurementApp.java file

--- src/main/java/com/quantity/app/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/app/QuantityMeasurementApp.java
@@ -1,20 +1,36 @@
-
 package com.quantity.app;
 
-import com.quantity.domain.length.Length;
 import com.quantity.domain.length.LengthUnit;
-import com.quantity.service.UnitConversionService;
+import com.quantity.domain.length.Quantity;
 
 public class QuantityMeasurementApp {
 
+    public static void demonstrateAddition(Quantity q1, Quantity q2) {
+        Quantity result = q1.add(q2);
+        System.out.println("Result: " + result);
+    }
+
+    public static void demonstrateConversion(double value, LengthUnit from,  LengthUnit to) {
+        double result = Quantity.convert(value, from, to);
+        System.out.println("Converted Value: " + result + " " + to);
+    }
+
     public static void main(String[] args) {
 
-        UnitConversionService service = new UnitConversionService();
+        //UC6 Examples
+        demonstrateAddition(
+                new Quantity(1.0, LengthUnit.FEET),
+                new Quantity(12.0, LengthUnit.INCHES)
+        );
 
-        Length feet = new Length(2, LengthUnit.FEET);
+        demonstrateAddition(
+                new Quantity(1.0, LengthUnit.YARDS),
+                new Quantity(3.0, LengthUnit.FEET)
+        );
 
-        Length result = service.convert(feet, LengthUnit.INCHES);
-
-        System.out.println("Converted Value: " + result);
+        demonstrateAddition(
+                new Quantity(2.54, LengthUnit.CENTIMETERS),
+                new Quantity(1.0, LengthUnit.INCHES)
+        );
     }
 }
