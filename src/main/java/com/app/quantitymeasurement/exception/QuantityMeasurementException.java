d98a50284ee8f869314410d525405912d2d2293c
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-03-16 11:56:44 +0530

    [Abhishek] : Implemented feature/UC16-JDBCPersistence branch

---
+++ src/main/java/com/app/quantitymeasurement/exception/QuantityMeasurementException.java
@@ -0,0 +1,10 @@
+package com.app.quantitymeasurement.exception;
+
+public class QuantityMeasurementException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public QuantityMeasurementException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
